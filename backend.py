ce0f62defd134c414f54d16c0900eafa1bd724d3
Eduardo Murillo <ed.murillo717@gmail.com>
2025-02-08 16:40:06 -0600

    Adding backend community

--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ CORS(app)
 # @app.route('/name', methods=['POST', 'GET','PUT','DELETE'])
 
 # Load Firebasekey JSON file
-cred = credentials.Certificate("spark-91799-firebase-adminsdk-fbsvc-988dacf8ee.json")
+cred = credentials.Certificate("spark-91799-firebase-adminsdk-fbsvc-c227b6885f.json")
 firebase_admin.initialize_app(cred)
 
 # Initialize Firestore (NoSQL Database)
@@ -21,11 +21,12 @@ def root():
 
 #this function captures the request to save the data to firebase
 @app.route('/save-settings', methods=['POST'])
-def save_layout():
+def save_settings():
     data = request.json
     profile_id = data.get('userID')
     
     db.collection("settings").document(profile_id).set(data)
+    return jsonify({"message": "Settings saved successfully"})
 
 #this gets the layout from the firebase, 
 # should be called when rendering profile page
@@ -37,7 +38,7 @@ def get_settings():
         return jsonify({'error': 'User ID Required'}),400
     
     doc = db.collection('settings').document(user_id).get()
-
+    print("ran")
     return jsonify(doc.to_dict())
 
 #this saves the theme for the user profile
@@ -45,7 +46,7 @@ def get_settings():
 def save_theme():
     data = request.json
     profile_id = data.get('userID')
-    db.collection("themes").document(profile_id).add(data)
+    db.collection("themes").document(profile_id).set(data)
     return jsonify({"message": "Theme saved successfully"})
 
     
@@ -60,6 +61,27 @@ def get_theme():
     doc = db.collection('themes').document(user_id).get()
 
     return jsonify(doc.to_dict())
+
+@app.route('/get-communities-info',methods=['GET'])
+def get_communities():
+    community_id = request.args.get('communities')
+
+    if not community_id:
+        return jsonify({'error': 'User ID Required'}),400
+    
+    doc = db.collection('communities').document(community_id).get()
+
+    return jsonify(doc.to_dict())
+
+@app.route('/add-community',methods=['POST'])
+def add_community():
+    community_id = request.arg.get("community")
+    data = request.json
+    if not community_id:
+        return jsonify({'error': 'User ID Required'}),400
+    
+    db.collection("collections").document(community_id).set(data)
+    return jsonify({"message": "Community saved successfully"})
 
 if __name__ == "__main__":
     app.run()
